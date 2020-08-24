1cfd7ff0caf7db2567dd80fdfa5d2409e7133e22
CamilaSantos <camscatt17@gmail.com>
2020-08-24 17:02:07 -0300

    Mudanças grupo de slides 3A

--- Universidade.cpp
+++ Universidade.cpp
@@ -1,8 +1,14 @@
 #include "Universidade.h"
 #include <string.h>
+#include <iostream>
+using std::cout;
+using std::endl;
 
-Universidade::Universidade(const char* nome){
-    strcpy(this->nome, nome);
+
+Universidade::Universidade(){
+    for(int i = 0; i<50; i++){
+        Dpto[i] = NULL;
+    }
 }
 
 Universidade::~Universidade(){
@@ -15,4 +21,15 @@ void Universidade::setNome(const char* nome){
 
 char* Universidade::getNome(){
     return this->nome;
+}
+
+void Universidade ::setDepartamento(Departamento* pdpto, int qtd){
+    Dpto[qtd]=pdpto;
+}
+
+void Universidade::imprimeDptos(){
+    for(int i = 0; i<50; i++ ){
+        if(Dpto[i] != NULL)
+            cout << Dpto[i]->getNomeDpto() << endl;
+    }
 }
--- Universidade.h
+++ Universidade.h
@@ -1,12 +1,18 @@
 #pragma once
+#include "Departamento.h"
 
 class Universidade {
     private:
         char nome[30];
+        Departamento* Dpto[50];
 
     public:
-        Universidade(const char* = "");
+        Universidade();
         ~Universidade();
+
         void setNome(const char* nome);
         char* getNome();
+        
+        void setDepartamento(Departamento* pdpto, int qtd);
+        void imprimeDptos();
 };
